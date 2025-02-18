5d1df3dec6209131e0aaddc0643e9514f58c0230
BHARATH <darknightcoder07@gmail.com>
2025-02-18 20:33:49 +0530

    works start

--- medical_chatbot/medical_chatbot/urls.py
+++ medical_chatbot/medical_chatbot/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from chatbot.views import chat
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('chatbot.urls')),
+     path('',chat,name='home'),
+    path('chat',chat,name='chat'),
+   
 ]
