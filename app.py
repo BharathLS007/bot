b931003d8213c227f204bd549abc132fdbcf6ca8
BHARATH <darknightcoder07@gmail.com>
2025-04-15 18:48:59 +0530

    changes

--- app.py
+++ app.py
@@ -401,6 +401,17 @@ def related_sym(psym1):
 def home():
     return render_template("home.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/service')
+def service():
+    return render_template('service.html')
 
 @app.route("/get")
 def get_bot_response():
@@ -738,4 +749,4 @@ if __name__ == "__main__":
     ran = ''.join(random.choices(string.ascii_uppercase + string.digits, k=S))
     # chat_sp()
     app.secret_key = str(ran)
-    app.run()
+    app.run(debug=True)
